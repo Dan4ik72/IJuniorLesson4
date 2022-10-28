ab2332f55c70c330ef66c45de7abdc02ad78c1c4
Dan4ik72 <103052910+Dan4ik72@users.noreply.github.com>
2022-10-28 14:21:10 +0300

    updated

--- Signaling/Signaling.cs
+++ Signaling/Signaling.cs
@@ -11,46 +11,10 @@ public class Signaling : MonoBehaviour
     private float _minVolume = 0;
     private float _maxVolume = 1;
     private float _volumeChangingStep = 0.03f;
-    private float _secondsToWaitInCoroutine = 0.2f;
-
+    
     private Coroutine _changingVolumeCoroutine;
 
-    private void Start()
-    {
-        _audioSource.volume = 0;        
-    }
-    
-    private IEnumerator IncreaseVolume()
-    {
-        if (_changingVolumeCoroutine != null)
-            StopCoroutine(_changingVolumeCoroutine);
-
-        var waitForSeconds = new WaitForSeconds(_secondsToWaitInCoroutine);
-
-        for (float i = _minVolume; i < _maxVolume; i += _volumeChangingStep)
-        {
-            _audioSource.volume += _volumeChangingStep;
-
-            yield return waitForSeconds;
-        }
-    }
-
-    private IEnumerator DecreaseVolumeAndStop()
-    {
-        if (_changingVolumeCoroutine != null)
-            StopCoroutine(_changingVolumeCoroutine);
-
-        var waitForSeconds = new WaitForSeconds(_secondsToWaitInCoroutine);
-
-        for (float i = _audioSource.volume; i > _minVolume; i -= _volumeChangingStep)
-        {
-            _audioSource.volume -= _volumeChangingStep;
-
-            yield return waitForSeconds;
-        }
-
-        _audioSource.Stop();
-    }
+    private WaitForSeconds _changingVolumeDelay = new WaitForSeconds(0.2f);
 
     public void StartAlarm()
     {
@@ -62,5 +26,42 @@ public class Signaling : MonoBehaviour
     public void StopAlarm()
     {
         _changingVolumeCoroutine = StartCoroutine(DecreaseVolumeAndStop());
+    }
+
+    private void Start()
+    {
+        _audioSource.volume = 0;        
+    }
+    
+    private IEnumerator IncreaseVolume()
+    {
+        TryToStopCurrentCoroutine();
+        
+        for (float i = _minVolume; i < _maxVolume; i += _volumeChangingStep)
+        {
+            _audioSource.volume += _volumeChangingStep;
+
+            yield return _changingVolumeDelay;
+        }
+    }
+
+    private IEnumerator DecreaseVolumeAndStop()
+    {
+        TryToStopCurrentCoroutine();
+
+        for (float i = _audioSource.volume; i > _minVolume; i -= _volumeChangingStep)
+        {
+            _audioSource.volume -= _volumeChangingStep;
+
+            yield return _changingVolumeDelay;
+        }
+
+        _audioSource.Stop();
+    }
+
+    private void TryToStopCurrentCoroutine()
+    {
+        if (_changingVolumeCoroutine != null)
+            StopCoroutine(_changingVolumeCoroutine);
     }
 }
